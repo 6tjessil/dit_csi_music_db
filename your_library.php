b49f72487ac02afbf0df0961f1cb766b67d99657
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-07-04 14:19:08 +1200

    Headers added
    I have added in headers

--- your_library.php
+++ your_library.php
@@ -15,13 +15,31 @@
 	?>
 	<div id="queryheader">
 		<h1>Cypher to Room</h1> 
-</div>
+	</div>
 <!--PHP STARTS HERE-->
 	<?php
         require_once("connect.php");
     ?>
 	<div class = "song_list">
 		<!--PHP STARTS HERE-->
+		<div class = "music_header">
+			Cypher
+		</div>
+		<div class = "music_header">
+			Surname
+		</div>
+		<div class = "music_header">
+			Christian
+		</div>
+		<div class = "music_header">
+			Form
+		</div>
+		<div class = "music_header">
+			Mentor
+		</div>
+		<div class = "music_header">
+			Role_id
+		</div>
 		<?php
 		//creates a variable to store the sql query
 		$query = ("SELECT *
@@ -32,61 +50,28 @@
 		while($output=mysqli_fetch_array($result))
 		{ 
 		?>
-			<div id = "music_list">
-				<?php echo $output["cypher"],$output["surname"];?> 
+			<div class = "music_list">
+				<?php echo $output["cypher"];?> 
+			</div>
+			<div class = "music_list">
+				<?php echo $output["surname"];?> 
+			</div>			
+			<div class = "music_list">
+				<?php echo $output["christian"];?> 
+			</div>			
+			<div class = "music_list">
+				<?php echo $output["form"];?> 
+			</div>			
+			<div class = "music_list">
+				<?php echo $output["mentor"];?> 
+			</div>			
+			<div class = "music_list">
+				<?php echo $output["role_id"];?> 
 			</div>
 		<?php
 		}
-		?>    
+		?> 
 	</div>    
 </body>
 </html>
 
-<!--<table>
-      <tr>
-        <th>Cypher</th>
-        <th>Room</th>
-      </tr>
-      ?php
-      require_once("connect.php");
-      $query = "SELECT t.cypher, r.room
-                    FROM teacher AS t
-                    JOIN teacher_to_room tr ON t.cypher = tr.cypher
-                    JOIN room r ON r.room_id = tr.room_id ";
-      $rs = mysqli_query($con, $query);
-      if ($rs) {
-        while ($row = mysqli_fetch_array($rs)) {
-          echo "<tr><td>" . $row['cypher'] . "</td><td>" . $row['room'] . "</td></tr>";
-        }
-      }
-      ?>
-    </table>
-
-
-
-
-
-    <div class="songs_list">
-    ?php
-        //creates a variable to store the sql query
-        $query = ("SELECT t.cypher, r.room
-                    FROM teacher AS t
-                    JOIN teacher_to_room tr ON t.cypher = tr.cypher
-                    JOIN room r ON r.room_id = tr.room_id ");
-        //Runs and stores the query using variables $con and $query
-        $result = mysqli_query($con,$query);
-        while($output=mysqli_fetch_array($result))
-        { 
-        ?>
-        <div id = "music_list">
-            ?php echo $output["cypher"],"$",
-            $output["surname"]," ",$output["christian"]," ",
-            $output["form"]," ",$output["mentor"]," 
-            ",$output["role_id"];?> 
-        </div>
-        ?php
-        }
-        ?>        
-		</div>
-    </div>
--->
