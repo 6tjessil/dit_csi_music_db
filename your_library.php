a35a0e69a879e4531c8fb25a920ae86f16b35aab
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-07-04 12:41:16 +1200

    Used grid system for music list
    Removed tables and created divs. Data is now displayed using CSS grid.

--- your_library.php
+++ your_library.php
@@ -3,26 +3,51 @@
 <html lang="en">
 
 <head>
-  <title>TJessil</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
+	<title>TJessil</title>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-  <?php
-  require("nav.php")
-  ?>
-  <div id="queryheader">
-    <h1>Cypher to Room</h1> 
-  </div>
-  <div class="content">
-    <table>
+	<?php
+	require("nav.php")
+	?>
+	<div id="queryheader">
+		<h1>Cypher to Room</h1> 
+</div>
+<!--PHP STARTS HERE-->
+	<?php
+        require_once("connect.php");
+    ?>
+	<div class = "song_list">
+		<!--PHP STARTS HERE-->
+		<?php
+		//creates a variable to store the sql query
+		$query = ("SELECT *
+				FROM teacher");
+		
+		//Runs and stores the query using variables $con and $query
+		$result = mysqli_query($con,$query);
+		while($output=mysqli_fetch_array($result))
+		{ 
+		?>
+			<div id = "music_list">
+				<?php echo $output["cypher"],$output["surname"];?> 
+			</div>
+		<?php
+		}
+		?>    
+	</div>    
+</body>
+</html>
+
+<!--<table>
       <tr>
         <th>Cypher</th>
         <th>Room</th>
       </tr>
-      <?php
+      ?php
       require_once("connect.php");
       $query = "SELECT t.cypher, r.room
                     FROM teacher AS t
@@ -36,7 +61,32 @@
       }
       ?>
     </table>
-  </div>
-</body>
 
-</html>
+
+
+
+
+    <div class="songs_list">
+    ?php
+        //creates a variable to store the sql query
+        $query = ("SELECT t.cypher, r.room
+                    FROM teacher AS t
+                    JOIN teacher_to_room tr ON t.cypher = tr.cypher
+                    JOIN room r ON r.room_id = tr.room_id ");
+        //Runs and stores the query using variables $con and $query
+        $result = mysqli_query($con,$query);
+        while($output=mysqli_fetch_array($result))
+        { 
+        ?>
+        <div id = "music_list">
+            ?php echo $output["cypher"],"$",
+            $output["surname"]," ",$output["christian"]," ",
+            $output["form"]," ",$output["mentor"]," 
+            ",$output["role_id"];?> 
+        </div>
+        ?php
+        }
+        ?>        
+		</div>
+    </div>
+-->
