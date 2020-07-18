81508c19212633d42a60000d2ce11b79dcbb62c0
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-07-18 16:25:21 +1200

    Responsive Nav
    *Nav collaspes when screen in shrunk. The links need to be styled after page is shrunk.

--- script.js
+++ script.js
@@ -29,4 +29,43 @@ document.getElementById("signupbtn").addEventListener("click", signupform);
 
 function signupform(){
   document.getElementById('signup_form').style.display='block'
+<<<<<<< Updated upstream
 }
+=======
+}
+
+const toggleSwitch = document.querySelector('.theme-switch input[type = "checkbox"]');
+
+function switchTheme(e) {
+  if(e.target.checked){
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if(currentTheme){
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if(currentTheme === 'dark'){
+    toggleSwitch.checked = true;
+  }
+
+}
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
+
+
+>>>>>>> Stashed changes
