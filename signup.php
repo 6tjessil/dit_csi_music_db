27b9536b6cbf827ad40fe267385e2bb3baa80b8e
6tjessil <67714344+6tjessil@users.noreply.github.com>
2020-07-21 09:39:48 +1200

    Finished
    Validation is completed

--- signup.php
+++ signup.php
@@ -5,8 +5,8 @@
     $username = $_POST['UserName']; 
     $password = $_POST['Password'];
     
-    $checkquery = "SELECT * FROM useraccount WHERE username = $username"; //Checks if username exsits
-    $insertquery = "INSERT INTO useraccount(`username`, `password`) VALUES ('$username','$password')"; //Inserts new user data
+    $checkquery = "SELECT * FROM user WHERE username = '$username'"; //Checks if username exsits
+    $insertquery = "INSERT INTO user(`username`, `password`) VALUES ('$username','$password')"; //Inserts new user data
                 
     $result_check = mysqli_query($con, $checkquery); //Running $insertquery
     $count = mysqli_num_rows($result_check); //Counts number of rows
@@ -16,7 +16,7 @@
         echo $output_message;
     } else{
         $result_query = mysqli_query($con,$insertquery); 
-        $output_message = "Name already taken";
+        $output_message = "Registration Successful";
         echo $output_message;
     }
 ?>
